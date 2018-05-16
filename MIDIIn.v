4122be6cb7e24520d8c23d9e8bf797ef59e442a8
Gusts Kaksis <gusts.kaksis@gmail.com>
2018-05-16 14:02:06 -0400

    Added main module, MIDI input and parser stubs

---
+++ MIDIIn.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: LU
+// Engineer: Gusts
+// 
+// Create Date:    13:41:59 05/16/2018 
+// Design Name:    fpga_synth
+// Module Name:    main 
+// Project Name: 
+// Target Devices: Spartan 3E Starter Kit + FX2-BB
+// Tool versions: 
+// Description: MIDI input module, handles raw UART data 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module MIDIIn(
+    );
+
+
+endmodule
---
+++ MIDIParse.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: LU
+// Engineer: Gusts
+// 
+// Create Date:    13:41:59 05/16/2018 
+// Design Name:    fpga_synth
+// Module Name:    main 
+// Project Name: 
+// Target Devices: Spartan 3E Starter Kit + FX2-BB
+// Tool versions: 
+// Description: MIDI serial data parser module
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module MIDIParse(
+    );
+
+
+endmodule
