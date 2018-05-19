a2eef4888bf51ade15817c819a2cd3062e090006
gupu <guntars.puzulis@hotmail.com>
2018-05-19 20:49:26 +0300

    Added implementation for convolutional filter

--- ConvolutionFilter.v
+++ ConvolutionFilter.v
@@ -24,6 +24,36 @@ module ConvolutionFilter(
 	
 	output [11:0] outSample
 );
-	// Not implemented
-	assign outSample = 0;
+	//localparam kernelType = 0;
+	localparam sampleSize = 12;
+	localparam bufferedSamples = 512;
+	localparam bufferSize = bufferedSamples * sampleSize;
+	
+	reg [bufferSize:0] sampleBuffer;// FIFO buffer of multiple samples
+	reg [11:0] sample = 0;// Resulting sample
+	reg [11:0] tempSample;// buffer for operating with one sample from sample buffer
+	reg [64:0] sampleSum;
+	
+	integer i;
+	integer j;
+	integer bitIter;
+	
+	always @(posedge inSampleReady) begin
+		sampleBuffer = sampleBuffer << 12;
+		sampleBuffer[11:0] = inSample;
+		
+		sample = 0;
+		bitIter = 0;
+		for(i = 0; i < bufferedSamples; i = i + 1) begin
+			// In Verilog the range must be bounded by constant expressions, so copy bit by bit, could use mask instead
+			for(j = 0; j < sampleSize; j = j + 1) begin
+				tempSample[j] = sampleBuffer[bitIter];
+				bitIter = bitIter + 1;
+			end
+			sampleSum = sampleSum + tempSample;
+		end
+		sampleSum = sampleSum / bufferedSamples;
+	end
+	
+	assign outSample = sample;
 endmodule
