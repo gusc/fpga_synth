82790ebfeb5057231be96ee611aa0e10b8fbebe8
gupu <guntars.puzulis@hotmail.com>
2018-06-17 12:06:09 +0300

    Added IIR low pass and IIR high pass filter types

--- ConvolutionFilter.v
+++ ConvolutionFilter.v
@@ -18,19 +18,49 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
+
+
+/* Filter calculation
+
+Using butterworth filters
+y - output sample
+x - input sample
+
+Recurrence relation:
+y[n] = (  
+		@finite response part: a coefficients in butterworth
+			a1 * x[n- 8])
+     + ( a2 * x[n- 7])
+     + ( a3 * x[n- 6])
+     + ( a4 * x[n- 5])
+     + ( a5 * x[n- 4])
+     + ( a6 * x[n- 3])
+     + ( a7 * x[n- 2])
+     + ( a8 * x[n- 1])
+     + ( a9 * x[n- 0])
+		@infinite response part: b coefficients in butterworth
+     + ( b1 * y[n- 8])
+     + ( b2 * y[n- 7])
+     + ( b3 * y[n- 6])
+     + ( b4 * y[n- 5])
+     + ( b5 * y[n- 4])
+     + ( b6 * y[n- 3])
+     + ( b7 * y[n- 2])
+     + ( b8 * y[n- 1])
+*/
 module ConvolutionFilter(
 	input signed [11:0] inSample,
 	input inSampleReady,
 	
-	// Filter types:
+	// Filter types: [IIR, X,X,X]
 	// No filter			- 0000
 	// low pass FIR		- 0001
 	// high pass FIR		- 0010
 	// band pass FIR		- 0011
 	// band reject FIR	- 0100
 	// Moving average		- 0101
-	// low pass IIR		- ????
-	// hight pass IIR		- ????
+	// low pass IIR		- 1001
+	// hight pass IIR		- 1010
 	input [3:0] inFilterType,
 	
 	output [11:0] outSample
@@ -48,6 +78,7 @@ module ConvolutionFilter(
 	reg signed [sampleSize - 1:0] sample = 0;// Resulting sample
 	reg signed [sampleSize - 1:0] tempSample = 0;// buffer for operating with one sample from sample buffer
 	reg signed [64:0] sampleSum;
+	reg signed [64:0] sampleIIRSum;
 	
 	integer i;
 	integer j;
@@ -153,12 +184,13 @@ module ConvolutionFilter(
 		sampleBuffer = sampleBuffer << 12;
 		sampleBuffer[11:0] = inSample;
 		sampleSum = 0;
+		sampleIIRSum = 0;
 		bitIter = 0;
 		
 		if(inFilterType == 4'b0000) begin // No filter applied
 			sample = inSample;
 		end
-		else if (inFilterType == 4'b0001 || inFilterType == 4'b0010) begin // Static low / high pass FIR filters
+		else if (inFilterType == 4'b0001 || inFilterType == 4'b1001 || inFilterType == 4'b0010 || inFilterType == 4'b1010) begin // Static low / high pass FIR filters
 			for(i = 0; i < singleCutoffSampleSpan; i = i + 1) begin
 				// In Verilog the range must be bounded by constant expressions, so copy bit by bit, could use mask instead
 				for(j = 0; j < sampleSize; j = j + 1) begin
@@ -174,9 +206,34 @@ module ConvolutionFilter(
 				sampleSum = sampleSum + tempSample;
 			end
 			// Normalize samples, cause they may get bigger than actual 12 bit value
-			sample = sampleSum / 128;
+			sampleSum = sampleSum / 128;
+			
 			IIRBuffer = IIRBuffer << 12;
-			IIRBuffer[11:0] = sample;
+			IIRBuffer[11:0] = sampleSum;
+			
+			// Calculate IIR
+			if(inFilterType == 4'b1001 || inFilterType == 4'b1010) begin
+				bitIter = 0;
+				for(i = 0; i < singleCutoffIIRSampleSpan; i = i + 1) begin
+					// In Verilog the range must be bounded by constant expressions, so copy bit by bit, could use mask instead
+					for(j = 0; j < sampleSize; j = j + 1) begin
+						tempSample[j] = sampleBuffer[bitIter];
+						bitIter = bitIter + 1;
+					end
+					if(inFilterType == 4'b0001) begin
+						tempSample = tempSample * IIRfilterLowPass500[i];
+					end
+					else if (inFilterType == 4'b0010) begin // high pass FIR filter
+						tempSample = tempSample * IIRfilterHighPass500[i];
+					end
+					sampleIIRSum = sampleIIRSum + tempSample;
+				end
+				sampleIIRSum = sampleIIRSum / 128;
+				sample = sampleSum + sampleIIRSum;
+			end	
+			else begin
+				sample = sampleSum;
+			end
 		end 
 		else if (inFilterType == 4'b0011 || inFilterType == 4'b0100) begin // Static band pass / reject FIR filters
 			for(i = 0; i < multiCutoffSampleSpan; i = i + 1) begin
